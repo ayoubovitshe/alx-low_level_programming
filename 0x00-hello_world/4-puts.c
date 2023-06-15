d47e15127e3dec0ca52e5469ffa7a98f46aba16a
ayoub chajari <aychajari@gmail.com>
2023-06-15 12:03:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prit fonction
+ *
+ * Return: always 0 (Success
+*/
+
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
